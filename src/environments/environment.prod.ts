4dcc325b097b9ca1ca36fc8848429a8f2eb5e6ae
Fanuel26 <56047301+fanuel26@users.noreply.github.com>
2023-03-28 07:31:06 +0000

    commit du 28/03/2023 à 07:31

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+
+  apiUrl: 'http://82.180.139.254:4300/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  apiUrl: 'http://82.180.139.254:4300/api/'
 };
 
 /*
